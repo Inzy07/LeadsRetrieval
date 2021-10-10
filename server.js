0ed6750b539aae73901d6102c42689916a78c9bd
Inzy07 <inzyayyaz@gmail.com>
2021-10-10 20:11:20 +0400

    Never expire facebook token

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodemailer = require("nodemailer");
 const app = express();
+const nodemailer = require("nodemailer");
 const port = 3000;
 
 // Enter the Page Access Token from the previous step
-const FACEBOOK_PAGE_ACCESS_TOKEN = 'EAAZAdI5IqWlkBAJtAPYNHxTd19pD475yEVjJ3BQqIYa4xiPZA5zLSJ2tzx8ielbXoeScaqJpgUVzewsrHWvEHjaRNePCCbFxLZAOyO19dTZA2XYjBSCvSZC60Leb7HyrKipTXpc0XNWp5RdMxIbv2LQLoMJZA88WOPOd8YxFdxCjcLperTNTA7oZBSPc7rarIcVnKGyZAJHItGuA6WhRu2xh';
+const FACEBOOK_PAGE_ACCESS_TOKEN = 'EAAZAdI5IqWlkBABzq5w0NlfvDhGj87KMp6ebQKlUXVryuVoerAAv8y3onNzf6P1ZBVWzDC2GmMBW0Dp17AVvqBkgrFvD9TL7ZAdejQ1NU5gQEzwC0bmG2gmZAcw8puS2cslDHPHAA0rfAEZAeJqJ7gV8R2JjPu6FEu96RWmbTxfoaZCQ2ZBTmqg';
 
 // Accept JSON POST body
 app.use(bodyParser.json());
